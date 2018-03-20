b32bb8acb7576ed83d6f8b7d05189ba397de3c68
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2018-03-20 12:33:03 +0000

    Include README.md in package

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Opus.Mixfile do
 
   defp package do
     [
-      files: ["lib", "mix.exs"],
+      files: ["README.md", "lib", "mix.exs"],
       maintainers: ["Dimitris Zorbas"],
       licenses: ["MIT"],
       links: %{github: "https://github.com/zorbash/opus"}
