99a530eb4f827529ae57008526c190c5b7a4f98c
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2018-04-26 00:50:46 +0100

    Implement Opus.Graph

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Opus.Mixfile do
       app: :opus,
       version: "0.3.1",
       elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
@@ -25,6 +26,10 @@ defmodule Opus.Mixfile do
   def application do
     [extra_applications: [:logger]]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
